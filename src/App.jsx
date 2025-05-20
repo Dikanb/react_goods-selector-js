43e2661297520472940a7aff2e91b33cca8bff61
Dikanb <dikanb1502@gmail.com>
2025-05-20 14:46:53 +0300

    add task solution jam remove

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export const goods = [
 ];
 
 export const App = () => {
-  const [selectedGood, setSelectedGood] = useState('Jam');
+  const [selectedGood, setSelectedGood] = useState('');
 
   return (
     <main className="section container">
